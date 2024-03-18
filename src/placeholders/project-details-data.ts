47d22b58f36fdccf8c0b77d711b3beff03cacf3c
sanditya12 <sanditya2002@gmail.com>
2024-03-19 00:11:29 +0700

    fixed build error

--- src/placeholders/project-details-data.ts
+++ src/placeholders/project-details-data.ts
@@ -135,43 +135,6 @@ const getProjectDetails = (): ProjectDetails[] => [
     ],
     techs: ["react", "redux", "nest", "mongodb", "cpp"],
   },
-  {
-    slug: "sorting-algorithm-visualization-in-terminal",
-    title: "Sorting Algorithms Visualization in Terminal",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry, also known as ipsum lorem",
-    mainImg: "/images/placeholders/page-image.png",
-    links: ["github.com/sanditya"],
-    contents: [
-      {
-        heading: "Background",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        img: "/images/placeholders/page-image.png",
-      },
-      {
-        heading: "Background",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        img: "/images/placeholders/page-image.png",
-      },
-      {
-        heading: "Background",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        img: "/images/placeholders/page-image.png",
-      },
-      {
-        heading: "Background",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        img: "/images/placeholders/page-image.png",
-      },
-    ],
-    techs: [
-      { name: "javascript", icon: "/images/tech-icons/javascript.png" },
-      { name: "java", icon: "/images/tech-icons/java.png" },
-      { name: "mysql", icon: "/images/tech-icons/mysql.png" },
-      { name: "nodejs", icon: "/images/tech-icons/nodejs.png" },
-      { name: "php", icon: "/images/tech-icons/php.png" },
-    ],
-  },
 ];
 
 export default getProjectDetails;
